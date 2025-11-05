08bd7678f72d18c1b4a71d7b54a6269021f56b97
kds20212962 <rlaehdtls@kookmin.ac.kr>
2025-11-05 19:50:27 +0900

    게임 시간 종료 구현
    시간 초과에 의한 게임 종료 구현

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject endgameText;
+    public Text timeText;
+   
+    private float leftTime;
+    private bool isGameover;
+
+    void Start()
+    {
+        leftTime = 10;
+        isGameover = false;
+    }
+
+
+    void Update()
+    {
+        if( leftTime <= 0 )
+        {
+            isGameover=true;
+        }
+        if (!isGameover)
+        {
+            leftTime -= Time.deltaTime;
+            timeText.text = (int)leftTime / 60 + " : " + (int)leftTime%60;
+        }
+        else
+        {
+            SceneManager.LoadScene("EndScene");
+        }
+    }
+
+    public void EndGame()
+    {
+        isGameover = true;
+        endgameText.SetActive(true);
+    }
+
+}
